2156c4197edc08618ff10fda6ea84ca32b526b3c
DeluxeLX <1784900074@qq.com>
2022-10-15 17:07:23 +0800

    更新了moodstore-pstest模块的pom文件

--- moodstore-pstest/src/main/java/com/kci/moodstore/pstest/PstestApplication.java
+++ moodstore-pstest/src/main/java/com/kci/moodstore/pstest/PstestApplication.java
@@ -1,13 +1,16 @@
 package com.kci.moodstore.pstest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class    PstestApplication {
+@Slf4j
+public class PstestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PstestApplication.class, args);
+        log.info("------------- moodstore-pstest模块加载成功 ---------------");
     }
 
 }
