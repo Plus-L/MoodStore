c8a2fcbaa7eece7fef2fcf68f93cd3884eea6fc9
JiaHao LI <2732457087@qq.com>
2022-10-15 14:47:17 +0800

    First Init

---
+++ moodstore-pstest/src/main/java/com/kci/moodstore/pstest/PstestApplication.java
@@ -0,0 +1,13 @@
+package com.kci.moodstore.pstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PstestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PstestApplication.class, args);
+    }
+
+}
---
+++ moodstore-pstest/src/test/java/com/kci/moodstore/pstest/PstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kci.moodstore.pstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PstestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
