1c1aabb31ca9f8c617ddfd75338d225b7d1cc2b9
Deluxe.LX <94706408+DeluxeLX@users.noreply.github.com>
2022-11-16 10:33:01 +0800

    Update OSSClientUtil.java

--- moodstore-framework/moodstore-framework-common/src/main/java/com/kci/moodstore/framework/common/util/OSSClientUtil.java
+++ moodstore-framework/moodstore-framework-common/src/main/java/com/kci/moodstore/framework/common/util/OSSClientUtil.java
@@ -7,8 +7,8 @@ public class OSSClientUtil {
 
     // 定义公开静态变量
     public static String END_POINT = "oss-cn-shenzhen.aliyuncs.com";
-    public static String ACCESS_KEY_ID = "LTAI5t9i7ZRBDUBwt4oZtP1m";
-    public static String ACCESS_KEY_SECRET = "xxmeHvNnKtRIcV0QZD5zhaFZXiEDTr";
+    public static String ACCESS_KEY_ID = "";
+    public static String ACCESS_KEY_SECRET = "";
     public static String BUCKET_NAME = "moodstore";
 
 }
